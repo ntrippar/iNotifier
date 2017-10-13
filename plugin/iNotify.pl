cc00eb0ca566f136f6ec9d9a75bdf336f26cd624
Nicolas Trippar <ntrippar@gmail.com>
2017-10-13 02:00:23 +0200

    fix

--- plugin/iNotify.pl
+++ plugin/iNotify.pl
@@ -1,4 +1,4 @@
-## /script load /path/iNotify.pl
+## /script load ~/iNotify.pl
 
 use strict;
 use Irssi;
@@ -56,25 +56,40 @@ sub notifier_it {
     my $current_nick = $server->{nick};
 
     # handle normal msgs, not private ones
-    if($channel ne $current_nick) {
+    if($current_nick ne $channel) {
       if ($notifier_on_nick && $data =~ m/$current_nick/) {
         $data = sprintf("%s: %s", $title, $data);
         $title = $channel;
         notify($server->{address}, $title, $data);
       }
-    }else{
-      notify($server->{address}, $title, $data);
     }
 }
 
-sub notifier_message {
+sub notifier_public_message {
   my ($server, $data, $nick, $mask, $target) = @_;
     notifier_it($server, $nick, $data, $target, $nick);
+            my ($server, $title, $data, $channel, $nick) = @_;
+
+    my $notifier_on_nick = Irssi::settings_get_str('notifier_on_nick');
+    my $current_nick = $server->{nick};
+
+    if ($notifier_on_nick && $data =~ m/$current_nick/) {
+        $data = sprintf("%s: %s", $nick, $data);
+        $title = $channel;
+        notify($server->{address}, $target, $data);
+    }
+
+  Irssi::signal_continue($server, $data, $nick, $mask, $target);
+}
+
+sub notifier_private_message {
+  my ($server, $data, $nick, $mask, $target) = @_;
+    notify($server->{address}, $nick, $data);
   Irssi::signal_continue($server, $data, $nick, $mask, $target);
 }
 
 
 Irssi::settings_add_str('misc', 'notifier_on_nick', 1);
-Irssi::signal_add('message public', 'notifier_message');
-Irssi::signal_add('message private', 'notifier_message');
+Irssi::signal_add('message public', 'notifier_public_message');
+Irssi::signal_add('message private', 'notifier_private_message');
 
